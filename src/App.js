4b309d28a4ee5849ea6f8901afdcb963b4dfef74
sohil <sohilsfc24@gmail.com>
2023-10-19 22:13:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+import { gsap } from "gsap";
 
-function App() {
+const App = () => {
+  let cursorEl = useRef(null);
+  let textRef = useRef(null);
+
+  useEffect(() => {
+    let cursorPoint = cursorEl.current;
+    let mouseMove = (e) => {
+      const { clientX, clientY } = e;
+      gsap.to(cursorPoint, { left: clientX, top: clientY });
+    };
+    let mouseEnter = (e) => {
+      gsap.to(cursorPoint, { scale: 5, duration: 0.2, ease: "power2.easeIn" });
+    };
+    let mouseLeave = (e) => {
+      gsap.to(cursorPoint, { scale: 1, duration: 0.2, ease: "power2.easeIn" });
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+    let textEnter = document.querySelector("h1");
+    textEnter.addEventListener("mouseenter", mouseEnter);
+    textEnter.addEventListener("mouseleave", mouseLeave);
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+      textEnter.removeEventListener("mouseenter", mouseEnter);
+      textEnter.removeEventListener("mouseleave", mouseLeave);
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="cursor" ref={cursorEl}></div>
+      <div className="text">
+        <h1 ref={textRef}>
+          We are a strategic design consultancy for change-makers in business.
+        </h1>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
